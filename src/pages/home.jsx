96d3dcb4c73b4845301906c93f07efaab02b52d7
Tobias R. <no@thankyou.com>
2026-09-24 11:18:45 +0200

    cleaned up CSS for home galleryboxes

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -9,38 +9,61 @@ export function Home(){
                 <div className="mw-games-buttons">
                     <nav>
                         <ul className="mw-games-nav">
-                            <Link to="/" ><button>Skyrim</button></Link>
-                            <Link to="" ><button>Oblivion</button></Link>
-                            <Link to="" ><button>Morrowind</button></Link>            
+                            <li>
+                                <Link to="/" ><button>Skyrim</button></Link>
+                            </li>
+                            <li>
+                                <Link to="" ><button>Oblivion</button></Link>
+                            </li>
+                            <li>
+                                <Link to="" ><button>Morrowind</button></Link>            
+                            </li>
                         </ul>
                     </nav>
                 </div>
             </div>
-            
-
-
-            <ul className="gallery">
-                <li>
-                    <Link className="gallerybox" to="/Dranleic_Haligdrake" title="Dranleic Haligdrake">
-                        <div className="thumb">
-                            <img src="/Dranleic-Haligdrake-thumb.jpg" alt="Dranleic Haligdrake" />
-                        </div>
-                        <div className="gallerytext">
-                            <p>Dranleic Haligdrake</p>
-                        </div>
-                    </Link>
-                </li>
-                <li>
-                    <Link className="gallerybox" to="/Pharis_Ironeye" title="Pharis Ironeye">
-                        <div className="thumb">
-                            <img src="/Pharis-Ironeye-thumb.jpg" alt="Pharis Ironeye" />
-                        </div>
-                        <div className="gallerytext">
-                            <p>Pharis Ironeye</p>
-                        </div>
-                    </Link>
-                </li>
-            </ul>
+        
+            {/* 
+                Gallery boxes are too big!
+                They need to be smaller and
+                 scale in size depending
+                on the size of the screen.
+                At 100% they are too big.
+            */}
+            <div className="character-nav">
+                <ul className="gallery">
+                    <li className="gallerybox">
+                        <Link to="/Dranleic_Haligdrake" title="Go to character page for Dranleic Haligdrake">
+                            <div className="thumb">
+                                <img src="/dummy-pic-1.png" alt="Dranleic Haligdrake" width="480" height="360"/>
+                            </div>
+                            <div className="gallerytext">
+                                <p>Dranleic Haligdrake</p>
+                            </div>
+                        </Link>
+                    </li>
+                    <li className="gallerybox">
+                        <Link to="/Pharis_Ironeye" title="Go to character page for Pharis Ironeye">
+                            <div className="thumb">
+                                <img src="/dummy-pic-1.png" alt="Pharis Ironeye" width="480" height="360"/>
+                            </div>
+                            <div className="gallerytext">
+                                <p>Pharis Ironeye</p>
+                            </div>
+                        </Link>
+                    </li>
+                    <li className="gallerybox">
+                        <Link to="" title="Go to character page for Priscilla Aurora">
+                            <div className="thumb">
+                                <img src="/dummy-pic-1.png" alt="Priscilla Aurora" width="480" height="360" />
+                            </div>
+                            <div className="gallerytext">
+                                <p>Priscilla Aurora</p>
+                            </div>
+                        </Link>
+                    </li>
+                </ul>
+            </div>
         </>
     )
 }
